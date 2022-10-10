414fc3a7144594acb66623a5a8a44918500f24fd
beeerlian <naufal.berlian.99@gmail.com>
2022-10-10 19:22:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const readline = require('readline');
+const internal = require('stream');
+const rl = readline.createInterface({
+       input: process.stdin,
+       output: process.stdout
+});
+const currency = new Intl.NumberFormat("id-ID", {
+       currency: "IDR",
+       style: "currency"
+})
+
+const calculateFee = (distance) => {
+
+       if (distance <= 2) {
+              return 8000;
+       }
+       else {
+              return distance * 5000;
+       }
+}
+
+const calculateServiceFee = (distance) => {
+       return distance * 0.045;
+}
+
+const calculate = (input) => {
+       distance = parseInt(input);
+       if (distance) {
+              const totalFee = calculateFee(distance);
+              const serviceFee = calculateServiceFee(totalFee);
+
+
+              console.log(`Fee : ${currency.format(totalFee)}`);
+              console.log(`Service Fee : ${currency.format(serviceFee)}`);
+              console.log(`Tekan enter untuk lanjut, q untuk keluar`);
+       }
+
+}
+
+const app = async () => {
+       let running = true;
+       let it = rl[Symbol.asyncIterator]();
+       // await it.next();
+       rl.setPrompt('Masukan Jarak : ')
+       rl.prompt()
+       for await (const line of it) {
+              if (!line) {
+                     rl.setPrompt('Masukan Jarak : ')
+                     rl.prompt()
+              }
+              if (line.toLowerCase() === 'q') {
+                     rl.setPrompt('Keluar')
+                     rl.prompt()
+                     running = false;
+                     break;
+              }
+              calculate(line);
+       }
+       while (running) {
+       }
+       rl.close();
+
+}
+
+
+app()
